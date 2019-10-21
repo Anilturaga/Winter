7f3ba0961f801d95469c6699d6899a0103533f6f
Anilturaga <31271852+Anilturaga@users.noreply.github.com>
2019-10-22 02:19:07 +0530

    Update collection.js

--- collection.js
+++ collection.js
@@ -1,18 +1,36 @@
-function teja(){
-    var info = 
-    {
-    id : JSON.parse(localStorage.getItem(1)),
-    id1 :JSON.parse(localStorage.getItem(2)),
-    id2 : JSON.parse(localStorage.getItem(3)),
-    id3 : JSON.parse(localStorage.getItem(4)),
-    id4 : JSON.parse(localStorage.getItem(5)),
-    id5 :JSON.parse(localStorage.getItem(6)),
-    id6 : JSON.parse(localStorage.getItem(7)),
-    id7 : JSON.parse(localStorage.getItem(8)),
-    id8 : JSON.parse(localStorage.getItem(9)),
-    id9 : JSON.parse(localStorage.getItem(10)),
-    id10 :JSON.parse(localStorage.getItem(11)),
-    };
+/*
+ * In the popup's scripts, running on <http://example.com>:
+ */
 
-    document.getElementById("teja").innerHTML = JSON.stringify(info);
+// Called sometime after postMessage is called
+function receiveMessage(event)
+{
+  // Do we trust the sender of this message?
+  if (event.origin !== "http://example.com:8080")
+      var info = 
+    {
+     0: JSON.parse(localStorage.getItem(1)),
+    1 :JSON.parse(localStorage.getItem(2)),
+    2 : JSON.parse(localStorage.getItem(3)),
+    3 : JSON.parse(localStorage.getItem(4)),
+    4 : JSON.parse(localStorage.getItem(5)),
+    5 :JSON.parse(localStorage.getItem(6)),
+    6 : JSON.parse(localStorage.getItem(7)),
+    7 : JSON.parse(localStorage.getItem(8)),
+    8 : JSON.parse(localStorage.getItem(9)),
+    9 : JSON.parse(localStorage.getItem(10)),
+    10 :JSON.parse(localStorage.getItem(11)),
+    };
+    
+  // event.source is window.opener
+  // event.data is "hello there!"
+
+  // Assuming you've verified the origin of the received message (which
+  // you must do in any case), a convenient idiom for replying to a
+  // message is to call postMessage on event.source and provide
+  // event.origin as the targetOrigin.
+  event.source.postMessage(JSON.stringify(info),
+                           event.origin);
 }
+
+window.addEventListener("message", receiveMessage, false);
